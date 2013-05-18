e15b539b7d9da894711197c3360701ed0060f8da
_pants <_pants@getlantern.org>
2013-05-18 08:00:01 -0400

    comment out demo section, tweak footer
    - use array for footer nav links to control ordering
    - ditch github developer wiki button, should be replaced with star and fork
      widgets
    - ditch 'some rights reserved'

--- app/scripts/controllers/footer.js
+++ app/scripts/controllers/footer.js
@@ -2,9 +2,9 @@
 
 angular.module('GetLanternSiteApp')
   .controller('FooterCtrl', ['$scope', function ($scope) {
-    $scope.navLinks = {
-      LINKTEXT_FORUMS: 'https://groups.google.com/group/lantern-users-en',
-      LINKTEXT_DOCS: 'https://github.com/getlantern/lantern/wiki',
-      LINKTEXT_DEVELOPERS: 'https://github.com/getlantern/lantern'
-    };
+    $scope.navLinks = [
+      {key: 'LINKTEXT_FORUMS', url: 'https://groups.google.com/group/lantern-users-en'},
+      {key: 'LINKTEXT_DOCS', url: 'https://github.com/getlantern/lantern/wiki'},
+      {key: 'LINKTEXT_DEVELOPERS', url: 'https://github.com/getlantern/lantern'}
+    ];
   }]);
