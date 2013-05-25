959f7c5774718e776089f9a12f2db471271c0965
_pants <_pants@getlantern.org>
2013-05-24 21:47:31 -0400

    sundry improvements

--- app/scripts/controllers/download.js
+++ app/scripts/controllers/download.js
@@ -3,12 +3,21 @@
 angular.module('GetLanternSiteApp')
   .controller('DownloadCtrl', function ($scope, osSniffer, installerDataFetcher) {
     $scope.selectedOS = osSniffer.os;
+    $scope.downloadClicked = false;
 
     installerDataFetcher.fetch().then(function (data) {
       $scope.data = data;
     });
 
+    $scope.handleDownload = function () {
+      $scope.downloadClicked = true;
+      if (!/nodownload/.test(window.location.search)) {
+        window.location.href = $scope.data[$scope.selectedOS].url;
+      }
+    };
+
     $scope.selectOS = function (os) {
+      $scope.downloadClicked = false;
       $scope.selectedOS = os;
     };
 
--- app/scripts/translations.js
+++ app/scripts/translations.js
@@ -3,10 +3,13 @@
 angular.module('GetLanternSiteApp', ['pascalprecht.translate'], ['$translateProvider',
   function ($translateProvider) {
     $translateProvider.translations('en_EN', {
+      LANTERN: 'Lantern',
+      BETA: 'Beta',
       FREE_THE_INTERNET: 'Free the Internet',
       SECURELY_SHARE: 'Securely share your connection to defeat censorship',
       CHECK_BACK_SOON: 'Check back again soon to download Lantern.',
       DOWNLOAD_LANTERN: 'Download Lantern',
+      CHOOSE_SYSTEM: 'Choose System',
       VERSION: 'Version',
       RELEASED: 'Released',
       OSX: 'OS X',
@@ -15,10 +18,13 @@ angular.module('GetLanternSiteApp', ['pascalprecht.translate'], ['$translateProv
       UBUNTU64: 'Ubuntu 64-bit',
       UNKNOWNOS: '(unknown operating system)',
       OTHER_SYSTEMS: 'Other systems',
-
-      LINKTEXT_FORUMS: 'Forums',
-      LINKTEXT_DOCS: 'Docs',
-      LINKTEXT_DEVELOPERS: 'Developers'
+      DOWNLOAD_IN_PROGRESS: 'Your download should be in progress...',
+      DIRECT_LINK: 'Direct download link',
+      WARNING: 'Warning: Lantern is pending external security audit and thorough peer review within the technical community. Please only use Lantern if you understand your risks.',
+      FORUMS: 'Forums',
+      DOCS: 'Docs',
+      DEVELOPERS: 'Developers',
+      COPYRIGHT: '© 2013 Brave New Software',
     });
 
     $translateProvider.preferredLanguage('en_EN');
