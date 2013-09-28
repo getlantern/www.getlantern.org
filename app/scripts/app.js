6294d7ca54ad9a63ec12c248ea4ad3d2a2c4c13c
_pants <_pants@getlantern.org>
2013-09-28 10:26:26 -0400

    replace github widgets with social media icons
    closes #11
    
    also switch from compass_twitter_bootstrap_awesome to compass_twitter_bootstrap,
    comment out _sprites.scss, and switch to a custom icon font generated by
    icomoon.io/app

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,13 +4,13 @@ angular.module('GetLanternSiteApp', [
     'pascalprecht.translate',
     'angular-google-analytics'
   ],
-  ['$translateProvider', 'constants', function ($translateProvider, constants) {
+  function ($translateProvider, constants) {
     if (!window.translations) {
       throw new Error('Expected "window.translations" to be populated. Was "grunt jsFromJSON" not run?');
       return;
     }
     $translateProvider.translations(window.translations[constants.DEFAULT_LOCALE]);
-  }])
-  .config(['AnalyticsProvider', 'constants', function (AnalyticsProvider, constants) {
+  })
+  .config(function (AnalyticsProvider, constants) {
     AnalyticsProvider.setAccount(constants.GA_ACCOUNT_ID);
-  }]);
+  });
