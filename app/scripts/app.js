849361cb36379a1f3e93b73e31a451f4a985ecee
_pants <_pants@getlantern.org>
2013-09-30 05:54:06 +0200

    basic support for locale negotiation closes #13

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,8 +15,34 @@ angular.module('GetLanternSiteApp', [
       $translateProvider.translations(langCode, transTable);
     });
     $translateProvider.useCookieStorage();
-    $translateProvider.preferredLanguage(constants.DEFAULT_LANGCODE);//XXX set this via locale negotiation with session.js
+    $translateProvider.preferredLanguage(negotiatedLang() || constants.DEFAULT_LANGCODE);
     $translateProvider.fallbackLanguage(constants.DEFAULT_LANGCODE);
+
+    function negotiatedLang() {
+      // requires session.js
+      var langPref = window.session && window.session.locale && window.session.locale.lang;
+      if (!langPref) {
+        return;
+      }
+      var countryPref = window.session.locale.country,
+          bestMatch;
+      for (var lc in constants.LANGS) {
+        var split = lc.split('_'),
+            langAvail = split[0];
+        if (langAvail === langPref) {
+          var countryAvail = angular.lowercase(split[1]);
+          if (countryAvail === countryPref) {
+            //console.debug('exact match:', lc);
+            return lc;
+          }
+          // if we had access to the list of the browser's weighted preferences
+          // we could do something smarter
+          bestMatch = bestMatch || lc;
+        }
+      }
+      //console.debug('best match:', bestMatch);
+      return bestMatch;
+    }
   }])
   .run(['$cookies', '$rootScope', '$translate', 'constants', function ($cookies, $rootScope, $translate, constants) {
     $rootScope.constants = constants;
