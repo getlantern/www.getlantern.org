045d2417315f0fe6b43bf70644cd4a219424d7fc
_pants <_pants@getlantern.org>
2014-01-03 21:24:02 -0500

    don't autoplay video on iOS
    closes #31

--- app/scripts/directives/videoOnClick.js
+++ app/scripts/directives/videoOnClick.js
@@ -2,13 +2,27 @@
 
 angular.module('lantern_www')
   .directive('videoOnClick', ['$rootScope', '$translate', '$window', 'constants', function ($rootScope, $translate, $window, constants) {
+
+    var autoPlay;
+    if ($window.player && $window.session) {
+      // only autoplay when not on iOS to prevent blocking playback
+      // (see https://github.com/getlantern/www.getlantern.org/issues/31)
+      autoPlay = $window.session.browser.os !== 'iPhone/iPod';
+    }
+
+    function maybePlayVideo() {
+      if (autoPlay) {
+        $window.player.playVideo();
+      }
+    }
+
     return function (scope, element) {
       element.bind('click', function () {
         var $lightbox = $rootScope.lightbox;
         $lightbox.removeClass('hide');
 
         if ($rootScope.initializedVideo) {
-          $window.player.playVideo();
+          maybePlayVideo();
         } else {
           var lightbox = $lightbox[0],
               $container = $lightbox.find('div'),
@@ -28,8 +42,8 @@ angular.module('lantern_www')
               videoId: '4TY4IVnMO98',
               playerVars: {
                 autohide: 1,
-                autoplay: 1,
-                cc_load_policy: 'en' === $translate.uses().substring(0, 2) ? 0 : 1,
+                autoplay: +autoPlay, // + converts bool to 1 or 0 appropriately
+                cc_load_policy: +('en' !== $translate.uses().substring(0, 2)),
                 controls: 2,
                 enable_js_api: 1,
                 origin: 'getlantern.org',
@@ -41,7 +55,7 @@ angular.module('lantern_www')
           };
 
           $window.onPlayerReady = function (event) {
-            event.target.playVideo();
+            maybePlayVideo();
           };
 
           $rootScope.initializedVideo = true;
