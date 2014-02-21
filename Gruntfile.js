db4833d37d39d4341b1faf3c8a5bdff539928234
_pants <_pants@getlantern.org>
2014-02-21 13:07:52 -0500

    configure compass with relativeAssets: true for dist target

--- Gruntfile.js
+++ Gruntfile.js
@@ -177,7 +177,8 @@ module.exports = function (grunt) {
       },
       dist: {
         options: {
-          generatedImagesDir: '<%= yeoman.dist %>/images/generated'
+          generatedImagesDir: '<%= yeoman.dist %>/images/generated',
+          relativeAssets: true
         }
       },
       server: {
