65e1c3cc2568326458e5a190f166149c62f007d3
Evan Zhou <evan@htmlfusion.com>
2014-04-07 11:07:18 -0700

    pushing new changes to signup section

--- app/scripts/controllers/signup.js
+++ app/scripts/controllers/signup.js
@@ -2,25 +2,24 @@
 
 angular.module('lantern_www')
   .controller('SignupCtrl', ['$scope',"$timeout",
-      function ($scope, $timeout) {
-          console.log("loaded sign up controller")
-          $scope.title="some title";
-          $scope.mc_email_class = "current";
-          $scope.mc_region_class = "";
-          
-          $scope.onEmailSubmit = function(){
-            $scope.mc_email_class = "";
+        function ($scope, $timeout) {
             
-            $timeout(function(){
-                $scope.mc_region_class = "current";
-            },500);
+            $scope.mc_email_class = "current";
+            $scope.mc_region_class = "";
+          
+            $scope.onEmailSubmit = function(){
+                $scope.mc_email_class = "";
+            
+                $timeout(function(){
+                    $scope.mc_region_class = "current";
+                },500);
           };
           
           $scope.onRegionSubmit = function(){
-              $scope.mc_region_class = "";
-              
-              $timeout(function(){
-                $scope.mc_list_class = "current";
-            },500);
+                $scope.mc_region_class = "";
+                  
+                $timeout(function(){
+                    $scope.mc_list_class = "current";
+                },500);
           }
     }]);
