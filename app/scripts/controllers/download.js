b1d18797ff474f84814b73308729b72fb2f47bc4
fffw <fffw@getlantern.org>
2015-03-26 12:04:12 +0800

    add tracking code to index.html

--- app/scripts/controllers/download.js
+++ app/scripts/controllers/download.js
@@ -55,6 +55,26 @@ angular.module('lantern_www')
     $scope.trackEvent = function(type) {
         $window.ga('send', 'event', type, 
                    'click', osSniffer.os);
+                   if (type === 'download') {
+                     $scope.conversionTrackingUrl = '//insight.adsrvr.org/tags/l2p03i8/ddylndam/iframe';
+                     $scope.trackFBConversion();
+                   }
+    };
+
+    $scope.trackFBConversion = function() {
+      (function() {
+        var _fbq = window._fbq || (window._fbq = []);
+        if (!_fbq.loaded) {
+          var fbds = document.createElement('script');
+          fbds.async = true;
+          fbds.src = '//connect.facebook.net/en_US/fbds.js';
+          var s = document.getElementsByTagName('script')[0];
+          s.parentNode.insertBefore(fbds, s);
+          _fbq.loaded = true;
+        }
+      })();
+      window._fbq = window._fbq || [];
+      window._fbq.push(['track', '6022956323756', {'value':'0.00','currency':'USD'}]);
     };
 
     $scope.selectOS = function (os) {
